ab4cbea2ff30562316549aa9cc51ac70e34f9391
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-25 04:51:09 +0000

    built different

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function calculate() {
 
   if (integerOneInt < 0) {
     document.getElementById("loop").innerHTML =
-      "Input a positive whole number!"
+      "nput a positive whole number!"
   } else {
     while (counter < integerOneInt) {
       console.log("Once through loop:" + counter)
