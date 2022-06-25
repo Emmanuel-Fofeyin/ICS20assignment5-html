c0ae8872d441f10a11a7286f65642e820544d19a
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-25 03:31:37 +0000

    1 modified file or folder

--- js/script.js
+++ js/script.js
@@ -41,8 +41,7 @@ function calculate() {
         total += -(NUMERATOR / denominator)
       }
 
-
-
+    
   counter++
     }
     document.getElementById("loop").innerHTML = "The number is " + total
